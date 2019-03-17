19df7b7f72bf5f5be3606537e910ea808cbaa6d3
malbiruk <2601074@gmail.com>
2019-03-17 20:15:05 +0300

    Add files via upload

---
+++ hw_B4.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import random
+
+
+number=random.randint(1,100)
+
+attempt1 = input('Угадайте число от 1 до 100: ')
+attempt1=int(attempt1)
+
+if attempt1 > number:
+    print('Меньше')
+elif attempt1 < number:
+    print('Больше')
+else:
+    print('Верно!')
+
+attempt=0
+while True:
+    attempt = input()
+    attempt=int(attempt)
+    if attempt > number:
+        print('Меньше')
+    elif attempt < number:
+        print('Больше')
+    else:
+        print('Верно!')
+        break
